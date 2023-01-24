91bc7557058cf6f53c3c1bb2a3eab7718dd98bce
Emma Forslund <emfo2102@student.miun.se>
2023-01-24 11:11:31 +0100

    Skapat funktionalitet på Razor-sidan

--- Labb1Razor/Pages/Razor.cshtml.cs
+++ Labb1Razor/Pages/Razor.cshtml.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
 
 namespace Labb1Razor.Pages
 {
     public class RazorModel : PageModel
     {
+
+
+
+
+
         public void OnGet()
         {
+
+
+
         }
     }
 }
